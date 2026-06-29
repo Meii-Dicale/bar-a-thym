b96f59517436bd2ab3c4f9d818b306fe6634a1c7
Lorelei Wannyn <loreleiwannyn@MacBook-Air-de-Lorelei.local>
2026-06-29 13:37:00 +0200

    Initialisation du projet Spring Boot — pom.xml, config sécurité et application.properties

---
+++ back/src/main/java/com/barathym/BarAThymApplication.java
@@ -0,0 +1,11 @@
+package com.barathym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarAThymApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BarAThymApplication.class, args);
+    }
+}

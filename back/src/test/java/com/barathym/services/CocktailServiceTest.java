c639965dee32c26bf75856a591a6ade427ee3597
Lorelei Wannyn <loreleiwannyn@MacBook-Air-de-Lorelei.local>
2026-07-01 15:38:00 +0200

    fix: mise à jour test CocktailService après ajout du champ ingredients au DTO

--- back/src/test/java/com/barathym/services/CocktailServiceTest.java
+++ back/src/test/java/com/barathym/services/CocktailServiceTest.java
@@ -42,7 +42,7 @@ class CocktailServiceTest {
         cocktail.setNom("Mojito");
         cocktail.setActif(true);
 
-        responseDTO = new CocktailResponseDTO(1L, "co_001", "Mojito", null, "Cocktail", true, null);
+        responseDTO = new CocktailResponseDTO(1L, "co_001", "Mojito", null, "Cocktail", true, null, List.of());
         requestDTO = new CocktailRequestDTO("co_001", "Mojito", null, "Cocktail", true, null);
     }
 
